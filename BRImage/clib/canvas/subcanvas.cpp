454c808ad89e794dc557efec4d33d45e5329c6d9
dustpancake <fergusbkr@gmail.com>
2020-10-15 18:01:01 +0200

    colour type specific diff functions, attempted friend implementation of subregion, but cannot specialize template friend

--- BRImage/clib/canvas/subcanvas.cpp
+++ BRImage/clib/canvas/subcanvas.cpp
@@ -1,1 +1,14 @@
 #include "subcanvas.hpp"
+/*
+template <>
+void subregion<Grey>(SubCanvas& subcanv, const Canvas &supercanv, int x0, int
+x1, int y0, int y1) noexcept {
+  // copy in row by row
+  for (int y = y0; y < y1; y++) {
+    memcpy(subcanv.layer + (y - y0) * subcanv.width,
+           supercanv.layer + (y * supercanv.width) + x0,
+           sizeof(PX_TYPE) * (x1 - x0));
+  }
+}
+
+*/
--- BRImage/clib/canvas/subcanvas.hpp
+++ BRImage/clib/canvas/subcanvas.hpp
@@ -22,14 +22,31 @@ public:
   virtual ~SubCanvas() noexcept override = default;
 
   template <class C>
-  constexpr void subregion(const Canvas &supercanv, int x0, int x1, int y0,
-                           int y1) noexcept {
-    for (int y = y0; y < y1; y++) {
-      for (int x = x0; x < x1; x++) {
-        this->update<C>(x - x0, y - y0, supercanv.get_px<C>(x, y));
-      }
+  friend constexpr void subregion(SubCanvas &subcanv, const Canvas &supercanv,
+                                  int x0, int x1, int y0, int y1) noexcept;
+};
+
+template <class C>
+constexpr void subregion(SubCanvas &subcanv, const Canvas &supercanv, int x0,
+                         int x1, int y0, int y1) noexcept {
+  for (int y = y0; y < y1; y++) {
+    for (int x = x0; x < x1; x++) {
+      subcanv.update<C>(x - x0, y - y0, supercanv.get_px<C>(x, y));
     }
   }
-};
+}
+
+/*
+template <>
+constexpr void subregion<Grey>(SubCanvas& subcanv, const Canvas &supercanv, int
+x0, int x1, int y0, int y1) noexcept {
+    // copy in row by row
+    for (int y = y0; y < y1; y++) {
+      memcpy(subcanv.layer + (y - y0) * subcanv.width,
+             supercanv.layer + (y * supercanv.width) + x0,
+             sizeof(PX_TYPE) * (x1 - x0));
+    }
+  }
+*/
 
 #endif

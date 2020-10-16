c18b664ce5e71f5152e44cd6840644752dcb46ea
Dustpancake <fergusbkr@gmail.com>
2020-10-16 05:34:02 +0200

    small quality of life fixes, and wrapped command line in main() method

--- BRImage/__main__.py
+++ BRImage/__main__.py
@@ -1,8 +1,11 @@
 from BRImage.commandline import cli
 
-if __name__ == "__main__":
+def main():
     args = cli.parse_args()
     if args.subcommand is None:
         print("No arguments supplied.")
     else:
         args.runner(args)
+
+if __name__ == "__main__":
+    main()
